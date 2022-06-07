6a626316e2af7941405e37535e800cf6f39b4c64
mireiasuefra <mireia.suefra@gmail.com>
2022-06-07 20:29:56 +0200

    add netlify

--- src/services/api-list.js
+++ src/services/api-list.js
@@ -1,6 +1,6 @@
 const callToApi = () => {
   return fetch(
-    "http://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
+    "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
   )
     .then((response) => response.json())
     .then((data) => {
