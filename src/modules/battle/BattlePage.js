0883ffde79eeefee6f663c3b31f14e105fb4fa20
mireiasuefra <mireia.suefra@gmail.com>
2022-06-20 21:19:46 +0200

    render code

--- src/modules/battle/BattlePage.js
+++ src/modules/battle/BattlePage.js
@@ -1,12 +1,10 @@
-import { useSelector } from "react-redux";
+
 import ButtonResetBattle from "./ButtonResetBattle";
 import SelectHero from "./SelectHero";
 import SelectWeapon from "./SelectWeapon";
 
 function BattlePage() {
-  const superHeroes = useSelector(
-    (state) => state.superHeroReducer.superHeroes
-  );
+ 
 
   return (
     <div className="battle-page">
